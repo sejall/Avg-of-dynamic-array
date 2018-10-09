5deb031c694cc5ab4de6f38fd4b78f8acd35a082
sejall <33982474+sejall@users.noreply.github.com>
2018-10-09 17:53:51 +0530

    Add files via upload

---
+++ Average of no.s using dynamic array/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,*a;
+    cout<<"Enter no. of elements you want in array:";
+    cin>>n;
+    a=new int[n];
+    cout << "Hello world!" << endl;
+    return 0;
+}
